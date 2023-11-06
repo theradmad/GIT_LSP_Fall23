9baea1f644d5c53893d83adf072fd5b3ba5a08e7
theradmad <madeline.bumpus@bison.howard.edu>
2023-11-06 17:44:06 -0500

    more robust test

--- LSP_Fall23/src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
+++ LSP_Fall23/src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
@@ -20,7 +20,7 @@ public class IntegerSetTest {
 	@Test
 	@DisplayName("test that a new, empty integer set is created")
     public void testConstructor() {
-        
+        assertFalse(setA.contains(9));
         assertTrue(setA.isEmpty()); 
     }
 	
@@ -28,9 +28,11 @@ public class IntegerSetTest {
     @DisplayName("Test case for add")
     public void testAdd() {
         
-        
+        setA.add(90);
         setA.add(5);
         assertTrue(setA.contains(5));
+        assertTrue(setA.contains(90));
+        assertFalse(setA.contains(56));
     }
     
     @Test
@@ -49,18 +51,18 @@ public class IntegerSetTest {
     @Test
     @DisplayName("Test case for contain")
     public void testContains() {
-        
+        setA.add(68);
         setA.add(78);
         assertTrue(setA.contains(78));
         assertFalse(setA.contains(67));
     }
     
     
-    
     @Test
     @DisplayName("Test case for empty")
     public void testEmpty() {
         setA.add(34);
+        setA.add(78);
         assertFalse(setA.isEmpty());
         setA.clear();
         assertTrue(setA.isEmpty());
@@ -70,7 +72,7 @@ public class IntegerSetTest {
     @Test
     @DisplayName("Test case for clear")
     public void testClear() {
-        
+        setA.add(7483);
     	setA.add(57);
         assertFalse(setA.isEmpty());
         setA.clear();
@@ -142,6 +144,8 @@ public class IntegerSetTest {
         setA.add(69);
         
         assertEquals("[5, 6, 78, 69]", setA.toString());
+        setA.clear();
+        assertEquals("[]", setA.toString());
     }
     
     @Test
@@ -202,6 +206,8 @@ public class IntegerSetTest {
     @DisplayName("Test case for set difference")
     public void testDiff() {
     	IntegerSet setB = new IntegerSet();
+    	IntegerSet setC = new IntegerSet();
+
     	setA.add(5);
         setA.add(6);
         setA.add(78);
@@ -210,10 +216,16 @@ public class IntegerSetTest {
         setB.add(6);
         setB.add(43);
         setB.add(98);
+        setC.add(5);
+        setC.add(8);
+        setC.add(42);
         setA.diff(setB);
+        setB.diff(setC);
         assertTrue(setA.contains(78));
         assertFalse(setA.contains(5));
         assertFalse(setA.contains(6));
+        assertFalse(setB.contains(5));
+        assertTrue(setB.contains(43));
     }
     
    
@@ -225,13 +237,23 @@ public class IntegerSetTest {
         setA.add(78);
         setA.add(69);
         IntegerSet setB = new IntegerSet();
+        IntegerSet setU = new IntegerSet();
         setB.add(2);
         setB.add(3);
         setB.add(78);
+        setU.add(789);
+        setU.add(3);
+        setU.add(34);
+        setU.add(33);
+        setU.add(30);
+        setU.add(69);
         setA.complement(setB);
+        setU.complement(setA);
         assertTrue(setA.contains(5));
         assertFalse(setA.contains(78));
         assertFalse(setA.contains(3));
+        assertTrue(setU.contains(3));
+        assertFalse(setU.contains(5));
     }
     
 }
